1ff6d553c63e0dc8a9bcaa94b068a67e53456cc3
Yeon Sik Cho <yeonsikcho@gmail.com>
2020-02-20 14:42:58 +0900

    edit

--- data/urls.py
+++ data/urls.py
@@ -6,5 +6,8 @@ app_name = 'data'
 
 urlpatterns = [
 	path('', spv.loadmain),
+	#Stockprice Related Links
 	path('stockprice/', spv.loadmain, name='stockprice'),
+	path('spvget_companylist/', spv.get_companylist, name='spvget_companylist'),
+	path('spvget_prices/', spv.get_prices, name='spvget_prices'),
 ]
--- data/views/stockprice.py
+++ data/views/stockprice.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from data.static.dbcon import get_con
+import json
+import pandas as pd
 
 def loadmain(request):
 	return render(request, 'stockprice.html')
+
+def get_companylist(request):
+	con, cursor = get_con()
+	cursor.execute("select * from companylist")
+	result = cursor.fetchall()
+	companylist = [f"{r[0]}: {r[1]}" for r in result]
+	con.close()
+	return JsonResponse({'companylist':companylist})
+
+def get_prices(request):
+	stock_list = json.loads(request.GET.get('stock_list',None))
+	start_dt, end_dt = json.loads(request.GET.get('dates',None))
+	con, cursor = get_con()
+	for i,stockid in enumerate(stock_list[1:]):
+		cursor.execute("select tradedate, cls_prc from stockprices where isu_cd = %s and tradedate>=%s and tradedate<=%s", [stockid, start_dt, end_dt])
+		if i == 0:
+			df = pd.DataFrame(list(cursor.fetchall()), columns = ['tradedate', stockid])
+		else:
+			df = pd.merge(df, pd.DataFrame(list(cursor.fetchall()), columns = ['tradedate', stockid]), how = "outer", on = "tradedate")
+	prices = [stock_list]+df.values.tolist()
+	return JsonResponse({'prices':prices})
