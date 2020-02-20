0676bce6c5a2ba361150d8a20aa84ccb05d578a0
Yeon Sik Cho <yeonsikcho@gmail.com>
2020-02-20 14:48:14 +0900

    dbcon edit

--- data/static/dbcon.py
+++
@@ -1,6 +0,0 @@
-import MySQLdb as sql
-
-def get_con():
-	con = sql.connect(host="eric-cho.com", port=3308, database="echo_blog", user="user_id", password="user_pw", charset="utf8")
-	cursor = con.cursor()
-	return con, cursor
