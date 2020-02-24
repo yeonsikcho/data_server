ef42a73d0a11846c6a425bac76510e46646f54ed
Yeon Sik Cho <yeonsikcho@gmail.com>
2020-02-24 17:51:13 +0900

    download price

--- data/urls.py
+++ data/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 import data.views.stockprice as spv 
 
+
 app_name = 'data'
 
 urlpatterns = [
@@ -10,4 +11,7 @@ urlpatterns = [
 	path('stockprice/', spv.loadmain, name='stockprice'),
 	path('spvget_companylist/', spv.get_companylist, name='spvget_companylist'),
 	path('spvget_prices/', spv.get_prices, name='spvget_prices'),
+	path('spvcreate_data/', spv.create_data, name='spvcreate_data'),
+	path('spvdownload_data/', spv.download_data, name='spvdownload_data'),
+
 ]
--- data/views/stockprice.py
+++ data/views/stockprice.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse, Http404
 from data.static.dbcon import get_con
 import json
 import pandas as pd
+import os
+import uuid
 
 def loadmain(request):
 	return render(request, 'stockprice.html')
@@ -27,3 +29,47 @@ def get_prices(request):
 			df = pd.merge(df, pd.DataFrame(list(cursor.fetchall()), columns = ['tradedate', stockid]), how = "outer", on = "tradedate")
 	prices = [stock_list]+df.values.tolist()
 	return JsonResponse({'prices':prices})
+	
+
+def create_data(request):
+	scope, frequency, format = json.loads(request.GET.get('parameters',None))[0]
+	start_dt, end_dt = json.loads(request.GET.get('download_dates',None))[0]
+	column_map = {'Opening Price':'open_prc','Low Price':'low_prc','High Price':'high_prc','Closing Price':'cls_prc','Adjusted Closing Price':'adj_prc','Shares Outstanding':'list_stock_vol'}
+	columns = ['isu_cd','tradedate']
+	for select, item in json.loads(request.GET.get('parameters2',None)):
+		if select:
+			columns.append(column_map[item])
+	conditions = []
+	if start_dt:
+		conditions.append(f"tradedate >= '{start_dt}'")
+	if end_dt:
+		conditions.append(f"tradedate <= '{end_dt}'")
+	if scope == 'KOSPI':
+		conditions.append(f"exchange = 'STK'")
+	if scope == 'KOSDAQ':
+		conditions.append(f"exchange = 'KSQ'")
+	if frequency == "monthly":
+		conditions.append(f"month_end = 1")
+	if frequency == "yearly":
+		conditions.append(f"year_end = 1")
+	con, cursor = get_con()
+	cursor.execute(f"select {', '.join(columns)} from stockprices where {' and '.join(conditions)}")
+	df = pd.DataFrame(list(cursor.fetchall()), columns = columns)
+	if format == "csv":
+		filename = str(uuid.uuid1())+".csv"
+		df.to_csv(f"files/{filename}", index = False)
+	if format == "excel":
+		filename = str(uuid.uuid(1))+".xlsx"
+		df.to_execl(f"files/{filename}", index = False)
+	return JsonResponse({'filename':filename})
+	
+def download_data(request):
+	file_path = "files/"+request.GET.get('path',None)
+	ext = file_path.split(".")[1]
+	print(file_path)
+	if os.path.exists(file_path):
+		with open(file_path, 'rb') as fh:
+			response = HttpResponse(fh.read(), content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+			response['Content-Disposition'] = f'attachment; filename="Stock_Prices.{ext}"'
+			return response
+	raise Http404
