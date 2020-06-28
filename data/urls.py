660c0393b433a05127c349dd30b0c142eada04b9
= <yeonsikcho@gmail.com>
2020-06-28 17:24:40 +0900

    The Data Incubator 12 Days

--- data/urls.py
+++ data/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 import data.views.stockprice as spv 
-
+import data.views.tdi12 as tdi12v 
 
 app_name = 'data'
 
@@ -14,4 +14,8 @@ urlpatterns = [
 	path('spvcreate_data/', spv.create_data, name='spvcreate_data'),
 	path('spvdownload_data/', spv.download_data, name='spvdownload_data'),
 
+	#The data incabuator 12 day related links
+	path('tdi12/', tdi12v.loadmain, name='tdi12'),
+	path('tdi12vkeyword_search/', tdi12v.keyword_search, name='tdi12vkeyword_search'),
+	path('tdi12vshow_plot/', tdi12v.show_plot, name='tdi12vshow_plot'),
 ]
